fe7aaa6bbd38e6322b0835a40aa802d4bec074c5
John Hawthorn <john@stembolt.com>
2016-02-22 01:47:13 -0800

    Use safe_join instead of html_safe where possible
    These methods were joining safe and unsafe strings and then considering
    the output safe.
    
    Especially concerning are display_line_item and display_variant which
    could likely allow a user with admin permissions to inject HTML
    unescaped into the admin.

--- backend/app/helpers/spree/admin/products_helper.rb
+++ backend/app/helpers/spree/admin/products_helper.rb
@@ -2,14 +2,15 @@ module Spree
   module Admin
     module ProductsHelper
       def taxon_options_for(product)
-        @taxons.map do |taxon|
+        options = @taxons.map do |taxon|
           selected = product.taxons.include?(taxon)
           content_tag(:option,
                       value: taxon.id,
                       selected: ('selected' if selected)) do
                         (taxon.ancestors.pluck(:name) + [taxon.name]).join(" -> ")
                       end
-        end.join("").html_safe
+        end
+        safe_join(options)
       end
 
       def option_types_options_for(product)
