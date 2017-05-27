d56b5932a370c3497bca5393c66fa792053e319e
Brian Buchalter <bal711@gmail.com>
2017-05-27 07:31:23 +0100

    Test order factories work with shipping method

--- core/spec/lib/spree/core/testing_support/factories/order_factory_spec.rb
+++ core/spec/lib/spree/core/testing_support/factories/order_factory_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 require 'spree/testing_support/factories/order_factory'
 
+shared_examples "shipping methods are assigned" do
+  context "given a shipping method" do
+    let(:shipping_method) { create(:shipping_method) }
+
+    it "assigns the shipping method when created" do
+      expect(
+        create(
+          factory,
+          shipping_method: shipping_method
+        ).shipments.all? do |shipment|
+          shipment.shipping_method == shipping_method
+        end
+      ).to be true
+    end
+
+    it "assigns the shipping method when built" do
+      expect(
+        build(
+          factory,
+          shipping_method: shipping_method
+        ).shipments.all? do |shipment|
+          shipment.shipping_method == shipping_method
+        end
+      ).to be true
+    end
+  end
+end
+
 RSpec.describe 'order factory' do
   let(:factory_class) { Spree::Order }
 
@@ -20,29 +48,34 @@ RSpec.describe 'order factory' do
     let(:factory) { :order_with_line_items }
 
     it_behaves_like 'a working factory'
+    it_behaves_like 'shipping methods are assigned'
   end
 
   describe 'completed order with totals' do
     let(:factory) { :completed_order_with_totals }
 
     it_behaves_like 'a working factory'
+    it_behaves_like 'shipping methods are assigned'
   end
 
   describe 'completed order with pending payment' do
     let(:factory) { :completed_order_with_pending_payment }
 
     it_behaves_like 'a working factory'
+    it_behaves_like 'shipping methods are assigned'
   end
 
   describe 'order ready to ship' do
     let(:factory) { :order_ready_to_ship }
 
     it_behaves_like 'a working factory'
+    it_behaves_like 'shipping methods are assigned'
   end
 
   describe 'shipped order' do
     let(:factory) { :shipped_order }
 
     it_behaves_like 'a working factory'
+    it_behaves_like 'shipping methods are assigned'
   end
 end
